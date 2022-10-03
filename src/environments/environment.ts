559f37806bcd0e38d9d9dfa2ab7f689253f742c7
DamienDark01 <105861697+DamienDark01@users.noreply.github.com>
2022-10-03 22:14:39 +0530

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
   XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
-  
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'c7993e98aemsh5875f9f5f4989c9p1fd948jsn9b00beb9af76'
 }
