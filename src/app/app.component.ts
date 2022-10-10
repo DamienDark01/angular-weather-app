e757d543d445a5829785537d372851e4df268565
DamienDark01 <105861697+DamienDark01@users.noreply.github.com>
2022-10-10 20:15:28 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,26 @@ export class AppComponent implements OnInit {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  cityName: string = 'Colombo';
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    });
+  }
 }
