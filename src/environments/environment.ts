b5af089aa533fd94447329486e7b4c62855af44a
DamienDark01 <105861697+DamienDark01@users.noreply.github.com>
2023-06-09 02:04:07 +0530

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml',
+  weatherApiBaseUrl: 'https://forecast9.p.rapidapi.com/',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIHostHeaderValue: 'forecast9.p.rapidapi.com',
   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
   XRapidAPIKeyHeaderValue: 'c7993e98aemsh5875f9f5f4989c9p1fd948jsn9b00beb9af76'
 }
